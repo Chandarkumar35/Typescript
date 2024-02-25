cf312eb9abe3def8880b3082b37f196dd6e4137e
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-02-25 12:36:29 +0500

    Add files via upload

---
+++ Names.ts
@@ -0,0 +1,8 @@
+let names :string[] = ['om', 'ram', 'kirshan','pirbhu'];
+
+const massage :string = 'wellcome to typescript calss: '
+for( let i=0; i<names.length; i++){
+
+console.log(massage + names[i])
+
+}

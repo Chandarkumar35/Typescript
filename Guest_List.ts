e6f6c35439ae2946ae8842bbfadfdfbbac477604
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-02-25 12:37:17 +0500

    Add files via upload

---
+++ Guest_List.ts
@@ -0,0 +1,8 @@
+let Guest_list :string[] =['Dada','jai', 'om'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+console.log('Dear Mr. ' + Guest_list[i] +  '\n\n\twe like you invite you for tea party. \n\nthank you\n');
+
+
+}

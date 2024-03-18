c33bf2dc7455567a0381328ad660e8b1a3e41fc7
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-03-18 12:55:01 +0500

    Add files via upload

---
+++ Alien_Color_3.ts
@@ -0,0 +1,51 @@
+
+// version-1
+let Alien_Color_3: string = 'green';
+
+if(Alien_Color_3 === 'green'){
+    console.log('version 1: the player just earned 5 point');
+
+} else if(Alien_Color_3 === 'yellow'){
+    console.log('the player earnd 10 points');
+
+} 
+else if(Alien_Color_3 === 'purple'){
+    console.log(' the player earned 15 points');
+}else{
+console.log('please select right color');
+
+}
+
+// version-2
+ Alien_Color_3 = 'yellow';
+ 
+if(Alien_Color_3 === 'green'){
+    console.log('the player just earned 5 point');
+
+} else if(Alien_Color_3 === 'yellow'){
+    console.log('the player earnd 10 points');
+
+} 
+else if(Alien_Color_3 === 'purple'){
+    console.log('the player earned 15 points');
+}else{
+console.log('please select right color');
+
+}
+// version-3
+Alien_Color_3 = 'purple'
+ 
+if(Alien_Color_3 === 'green'){
+    console.log('the player just earned 5 point');
+
+} else if(Alien_Color_3 === 'yellow'){
+    console.log('the player earnd 10 points');
+
+} 
+else if(Alien_Color_3 === 'purple'){
+    console.log('the player earned 15 points');
+}else{
+console.log('please select right color');
+
+}
+

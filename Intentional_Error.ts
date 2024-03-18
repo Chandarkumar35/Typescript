e29e3b313d7bf2ff08035bcb3fadc8701627aab9
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-03-18 12:52:40 +0500

    Add files via upload

---
+++ Intentional_Error.ts
@@ -0,0 +1,12 @@
+// if you havent received an array index error in one of your programs yet, try to make one happen.
+
+// creating array
+let newArray :String [] = ['om', 'ram', 'seeta'];
+
+//accessing person arry
+console.log(newArray[5]);
+
+
+// correct the error 
+
+console.log(newArray[2]);

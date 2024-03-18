8295124ddf210243d284222d97a47bd5323ff747
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-03-18 12:58:13 +0500

    Add files via upload

---
+++ animal.ts
@@ -0,0 +1,5 @@
+let animal: string[] = ['cow', 'cat', 'dog','rabait','rat', 'peacock'];
+for(let ani of animal){
+    console.log(`A ${ani} is great Pet`);
+}
+console.log(`Any of these animals would make a great pet`);

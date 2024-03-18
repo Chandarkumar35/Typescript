dc75a30cbfde07d7d399d65adbf1a52007f51e6f
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-03-18 12:54:24 +0500

    Add files via upload

---
+++ alienColors2 .ts
@@ -0,0 +1,26 @@
+// alien color #02: choose a color form alien as you did in exe-25 
+let aleinColors2: string = 'green';
+
+// write if else chain
+// if the alien color is green
+//print a statement that player earn 5 points
+
+if(aleinColors2 === 'green'){
+console.log('player earned the 5 points for shooting the alein');
+
+} else{
+console.log('player just earned 10 points');
+
+}
+// if the elein colors isnot green,  print a statement that the players earned 10 points 
+
+aleinColors2 = "yellow";
+ if(aleinColors2 === 'green'){
+    console.log('the player earned 5 points');
+
+
+ } else{
+    console.log('the player just earned 10 points');
+ }
+
+ 

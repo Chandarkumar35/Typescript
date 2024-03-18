cb2c94ec40fa5ceaa2c2e415898630786a34dd01
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-03-18 12:54:02 +0500

    Add files via upload

---
+++ alienColors.ts
@@ -0,0 +1,18 @@
+// alien colors #1. imagine an alien was just shot down in a game. 
+//create a varialbe called alien_color and assign it a value of 'green','yellow' or 'red'.
+
+//create a varialbe called alien_color
+let alien_color :string = 'green';
+
+// 
+if(alien_color === 'green'){
+console.log('Player just earned 5 points !');
+
+}
+
+alien_color = 'yellow';
+if(alien_color === 'green'){
+console.log('you have earned 5 points');
+
+
+}

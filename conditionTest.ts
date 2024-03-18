b86c52e371307465a0d6a41c92bd7999c778f375
Chandarkumar35 <160410322+Chandarkumar35@users.noreply.github.com>
2024-03-18 12:53:20 +0500

    Add files via upload

---
+++ conditionTest.ts
@@ -0,0 +1,46 @@
+//  write a serues if conditional tests.
+
+// creating a car varible 
+let car: string = 'civic';
+
+// test No 1
+console.log("is car == 'civic'? I predict Ture.");
+console.log(car == 'civic');
+
+// = hm kisi value ko set kr rhe h 
+// == hm kisi value ko check kr rhy h 
+// === hm kisi value ko uski datatype k sath check kr rhy h 
+// != Not Equal to 
+// 2 != 2 es ka result false ae ga
+
+// test No 2
+console.log("is car === 'civic'? I predict Ture.");
+console.log(car === 'civic');
+
+// test No 3
+console.log("is car != 'Honda'? I predict Ture.");
+console.log(car != 'Honda');
+
+// test No 4
+console.log("is car !== 'fort'? I predict Ture.");
+console.log(car !== 'fort');
+
+// test No 5
+console.log("is car.UPER Case == 'CIVIC'? I predict Ture.");
+console.log(car.toUpperCase() == 'CIVIC');
+
+// test No 6
+console.log("is car.Lower Case == 'CIVIC'? I predict False.");
+console.log(car.toLowerCase() == 'CIVIC');
+
+// test No 6
+console.log("is car === 'CIVIC'? I predict False.");
+console.log(car === 'CIVIC');
+
+// test No 7
+console.log("is car == 'Train'? I predict False.");
+console.log(car == 'Train');
+
+// test No 8,9,10
+console.log("is car == 'cycle'? I predict False.");
+console.log(car == 'cycle');
